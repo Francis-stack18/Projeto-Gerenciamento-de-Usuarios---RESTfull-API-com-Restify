de90d9b7dec6c6db4597e57deba8ff079e7b1787
Francis <francis13322@gmail.com>
2026-05-15 22:55:50 -0300

    U10 - Refatorando para Fetch API

--- client-server/public/models/User.js
+++ client-server/public/models/User.js
@@ -70,7 +70,7 @@ class User {
 
   static getUsersStorage() {
     
-    return HttpRequest.get("/users");
+    return Fetch.get("/users");
 
   }
 
@@ -89,9 +89,9 @@ class User {
       let promise;
 
       if (this.id) {
-        promise = HttpRequest.put(`/users/${this.id}`, this.toJSON());
+        promise = Fetch.put(`/users/${this.id}`, this.toJSON());
       } else {
-        promise = HttpRequest.post(`/users`, this.toJSON());
+        promise = Fetch.post(`/users`, this.toJSON());
       }
 
       promise
@@ -107,6 +107,6 @@ class User {
   }
 
   remove() {
-    return HttpRequest.delete(`/users/${this.id}`);
+    return Fetch.delete(`/users/${this.id}`);
   }
 }
